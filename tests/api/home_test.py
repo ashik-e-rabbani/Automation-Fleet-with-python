8761e90f3f0f9f0e7bb7e47b0e54c35803dc240a
Ashik Rabbani <ashik.ashik.rabbni@gmail.com>
2024-01-14 11:34:16 +0600

    Running the test with pytest
    Running the test with pytest
    Assertion

--- tests/api/home_test.py
+++ tests/api/home_test.py
@@ -2,14 +2,14 @@ import pytest
 import requests
 
 # Define the base URL for your API
-BASE_URL = "https://rapidapi.com"
+BASE_URL = "http://10.9.0.77:6060/np-payment-gateway/"
 
 def test_get_request():
     # Send a GET request to the API endpoint
-    response = requests.get(BASE_URL + "/blog/api-glossary/api-request/")
+    response = requests.get(BASE_URL + "api/admin/reconcile-dbbl/12-12-2023 10:02:15/11-11-2024 10:02:15")
 
     # Assert the response status code
-    assert response.status_code == 200
+    assert response.status_code == 201
 
     # Add more assertions based on your API response
 
